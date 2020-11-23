e7ebd79d6f31531f4e7029cfad897239c36a1ee3
Tomas Bellus <tomas.bellus@gmail.com>
2020-11-23 15:39:47 +0100

    change pg timezone to system timezone

--- setup_timescaledb.sh
+++ setup_timescaledb.sh
@@ -2,5 +2,6 @@
 
 database="brewmaster"
 
+psql -U postgres -c "SET TIME ZONE DEFAULT"
 psql -U postgres -c "CREATE DATABASE $database;"
 psql -U postgres -d "$database" -c 'CREATE EXTENSION "uuid-ossp";'
